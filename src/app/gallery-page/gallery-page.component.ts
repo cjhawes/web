79010e7a0764b6c40292211c33ee8145dc2618f2
Connor <cjhawes.cs@gmail.com>
2020-05-05 18:15:32 +0100

    Final prelim deployment

--- src/app/gallery-page/gallery-page.component.ts
+++ src/app/gallery-page/gallery-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../services/image.service';
+import { Image } from '../Models/image.model';
 
 @Component({
   selector: 'app-gallery-page',
@@ -7,7 +8,7 @@ import { ImageService } from '../services/image.service';
   styleUrls: ['./gallery-page.component.css']
 })
 export class GalleryPageComponent implements OnInit {
-  images;
+  images: Image[];
 
   constructor(
     private imageService: ImageService
